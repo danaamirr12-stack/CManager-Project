f1113c5497bf4972c1d36c1d953ec460452f3a14
dana <danaamirr12@gmail.com>
2025-12-10 14:16:39 +0100

    Initial commit

--- CManager.Presentation.ConsoleApp/Program.cs
+++ CManager.Presentation.ConsoleApp/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// CManager - Customer Management System
+// Dana Amir
+
+namespace CManager.Presentation.ConsoleApp
+{
+    internal class Program
+    {
+        // resten av koden...
